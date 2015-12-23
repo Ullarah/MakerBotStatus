3fa6fa303e34e9dc4df567caec3fd00ed10969b9
Ullarah <phil@mcau.ullarah.com>
2015-12-24 08:14:13 +1000

    Multiple printer support Use .auth file to add IP and KEY TODO: Auto place key in .auth file

--- makerbot.js
+++ makerbot.js
@@ -1,119 +1,162 @@
-﻿var json;
+﻿var printers = [];
 
 function start() {
 
-    var img = new Image();
+    parseJSON('makerbot.auth', function(json) {
 
-    img.onload = function () {
-        document.getElementById("webcam").getContext("2d").drawImage(this, 0, 0);
-    }
+        json.printers.forEach(function(prn) {
 
-    updateStats();
+            var ip_regex = /(?:\d{1,3}\.\d{1,3}\.)(\d{1,3}\.\d{1,3})/g;
+            var ip_octet = ip_regex.exec(prn.ip);
+            printers.push(ip_octet[1]);
 
-    img.src = "makerbot.png" + "?t=" + new Date().getTime();
+            document.body.innerHTML += getPrinterContainer(ip_octet[1]);
 
-    setTimeout("start()", 250);
+        });
+
+        printerStats();
+
+    });
 
 }
 
-function readJSON() {
+function printerStats() {
 
-    var JSONFile = new XMLHttpRequest();
+    printers.forEach(function(prn) {
 
-    JSONFile.open("GET", "makerbot.json", true);
+        var img = new Image();
 
-    JSONFile.onreadystatechange = function () {
+        parseJSON('makerbot-' + prn + '.json', function(json) {
+            updateStats(prn, json);
+        });
 
-        if (JSONFile.readyState === 4) {
+        img.onload = function () {
+            document.getElementById("webcam-" + prn).getContext("2d").drawImage(this, 0, 0);
+        }
 
-            if (JSONFile.status === 200 || JSONFile.status === 0) {
+        img.src = "makerbot-" + prn + ".png" + "?t=" + new Date().getTime();
 
-                json = JSON.parse(JSONFile.responseText || "null");
+    });
 
-            }
+    setTimeout("printerStats()", 250);
+
+}
+
+function parseJSON(file, callback) {
+
+    var rawFile = new XMLHttpRequest();
+
+    rawFile.open("GET", file);
+
+    rawFile.onreadystatechange = function () {
+
+        if (rawFile.readyState == 4 && rawFile.status === 200) {
+
+            return callback(JSON.parse(rawFile.responseText));
 
         }
 
     }
 
-    JSONFile.send(null);
+    rawFile.send(null);
 
 }
 
-function updateStats() {
+function getPrinterContainer(ip) {
 
-    readJSON();
+    var container = "<div id='container-" + ip + "'>";
 
-    if (json == null) { return; }
+    container += "<canvas id='webcam-" + ip + "' width='320' height='240'></canvas>";
+    container += "<div id='progress-" + ip + "'><div id='bar-" + ip + "'></div><div id='counter-" + ip + "'><span id='print_progress-" + ip + "'>Loading ...</span></div></div>";
+    container += "<span class='name' id='print_name-" + ip + "'>&nbsp;</span>";
+    container += "<div class='stat'><span class='stat_type'>Extruder:</span><span class='result' id='temp-" + ip + "'>--</span></div>";
+    container += "<div class='stat'><span class='stat_type'>Elapsed Time:</span><span class='result' id='time_current-" + ip + "'>--</span></div>"
+    container += "<div class='stat'><span class='stat_type'>Estimated Time:</span><span class='result' id='time_target-" + ip + "'>--</span></div>"
+    container += "<div class='stat'><span class='stat_type'>Material Use:</span><span class='result' id='material_use-" + ip + "'>--</span></div>"
+    container += "<div class='stat'><span class='stat_type'>Printer:</span><span class='result' id='printer-" + ip + "'>--</span></div></div>";
+
+    return container;
+
+}
+
+function updateStats(ip, bot) {
 
     var regex = /\/.*\/(.*?)\./g;
 
-    document.getElementById("printer").innerHTML = json.result.machine_name;
-    document.title = json.result.machine_name;
+    document.getElementById("printer-" + ip).innerHTML = bot.result.machine_name;
 
     var currentTime = new Date(null);
     var targetTime = new Date(null);
 
-    var progress_bar    = document.getElementById("bar");
-    var progress_status = document.getElementById("print_progress");
-    var print_name      = document.getElementById("print_name");
-    var temperature     = document.getElementById("temp");
-    var time_current    = document.getElementById("time_current");
-    var time_target     = document.getElementById("time_target");
-    var material_use    = document.getElementById("material_use");
+    var progress_bar    = document.getElementById("bar-" + ip);
+    var progress_status = document.getElementById("print_progress-" + ip);
+    var print_name      = document.getElementById("print_name-" + ip);
+    var temperature     = document.getElementById("temp-" + ip);
+    var time_current    = document.getElementById("time_current-" + ip);
+    var time_target     = document.getElementById("time_target-" + ip);
+    var material_use    = document.getElementById("material_use-" + ip);
 
     time_current.innerHTML    = "--";
     time_target.innerHTML     = "--";
 
-    temperature.innerHTML     = json.result.toolheads.extruder[0].current_temperature + "&deg;C";
+    temperature.innerHTML     = bot.result.toolheads.extruder[0].current_temperature + "&deg;C";
 
-    if (json.result.current_process == null) {
+    if (bot.result.current_process == null) {
         progress_status.innerHTML = "Ready to Print";
         print_name.innerHTML      = "&nbsp";
         return;
     }
 
-    progress_bar.style.width  = json.result.current_process.progress + '%';
-    progress_status.innerHTML = json.result.current_process.progress + "%";
+    progress_bar.style.width  = bot.result.current_process.progress + '%';
+    progress_status.innerHTML = bot.result.current_process.progress + "%";
 
-    currentTime.setSeconds(json.result.current_process.elapsed_time);
-    targetTime.setSeconds(json.result.current_process.time_estimation);
+    currentTime.setSeconds(bot.result.current_process.elapsed_time);
+    targetTime.setSeconds(bot.result.current_process.time_estimation);
 
-    print_name.innerHTML = regex.exec(json.result.current_process.filename)[1].substring(0, 10);
+    print_name.innerHTML = regex.exec(bot.result.current_process.filename)[1].substring(0, 10);
 
-    var filament = json.result.current_process.extrusion_mass_g;
-    material_use.innerHTML = filament + "g (" + (json.result.current_process.extrusion_mass_g*0.0022).toFixed(2) + "lb)";
+    var filament = bot.result.current_process.extrusion_mass_g;
+    material_use.innerHTML = filament + "g (" + (bot.result.current_process.extrusion_mass_g*0.0022).toFixed(2) + "lb)";
 
-    switch (json.result.current_process.step) {
+    switch (bot.result.current_process.step) {
 
         case 'clear_build_plate':
             progress_status.innerHTML = "Clear Build Plate " + progress_status.innerHTML;
-            temperature.innerHTML     = temperature.innerHTML + " (" + json.result.toolheads.extruder[0].target_temperature + "&deg;C)";
+            material_use.innerHTML    = "--";
+            break;
+
+        case 'transfer':
+            progress_status.innerHTML = "Transfer " + progress_status.innerHTML;
+            material_use.innerHTML    = "--";
             break;
 
         case 'initial_heating':
             progress_status.innerHTML = "Initial Heating " + progress_status.innerHTML;
-            temperature.innerHTML     = temperature.innerHTML + " (" + json.result.toolheads.extruder[0].target_temperature + "&deg;C)";
+            temperature.innerHTML     = temperature.innerHTML + " (" + bot.result.toolheads.extruder[0].target_temperature + "&deg;C)";
+            material_use.innerHTML    = "--";
             break;
 
         case 'homing':
             progress_status.innerHTML = "Homing " + progress_status.innerHTML;
-            temperature.innerHTML     = temperature.innerHTML + " (" + json.result.toolheads.extruder[0].target_temperature + "&deg;C)";
+            temperature.innerHTML     = temperature.innerHTML + " (" + bot.result.toolheads.extruder[0].target_temperature + "&deg;C)";
+            material_use.innerHTML    = "--";
             break;
 
         case 'position_found':
             progress_status.innerHTML = "Position Found " + progress_status.innerHTML;
-            temperature.innerHTML     = temperature.innerHTML + " (" + json.result.toolheads.extruder[0].target_temperature + "°C)";
+            temperature.innerHTML     = temperature.innerHTML + " (" + bot.result.toolheads.extruder[0].target_temperature + "°C)";
+            material_use.innerHTML    = "--";
             break;
 
         case 'final_heating':
             progress_status.innerHTML = "Final Heating " + progress_status.innerHTML;
-            temperature.innerHTML     = temperature.innerHTML + " (" + json.result.toolheads.extruder[0].target_temperature + "°C)";
+            temperature.innerHTML     = temperature.innerHTML + " (" + bot.result.toolheads.extruder[0].target_temperature + "°C)";
+            material_use.innerHTML    = "--";
             break;
 
         case 'printing':
             progress_status.innerHTML = "Printing " + progress_status.innerHTML;
-            temperature.innerHTML     = temperature.innerHTML + " (" + json.result.toolheads.extruder[0].target_temperature + "°C)";
+            temperature.innerHTML     = temperature.innerHTML + " (" + bot.result.toolheads.extruder[0].target_temperature + "°C)";
             time_current.innerHTML    = currentTime.toISOString().substr(11, 8);
             time_target.innerHTML     = targetTime.toISOString().substr(11, 8);
             break;
@@ -126,8 +169,4 @@ function updateStats() {
 
     }
 
-}
-
-function kToLbs(pK) {
-    return Math.floor(pK/0.45359237);
 }
--- makerbot.py
+++ makerbot.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
-from SimpleHTTPServer import SimpleHTTPRequestHandler
-from BaseHTTPServer import HTTPServer as ServerClass
 from socket import error as socket_error
+from collections import namedtuple
 import threading
 import logging
 import json
@@ -9,38 +8,45 @@ import makerbotapi
 import sys
 import time
 import os
+import re
 
-
-ip = '192.168.64.66'
-port = 80
 
 httpd = None
 makerbot = None
+connection = []
 
 pid = str(os.getpid())
 pidfile = os.getcwd() + "//makerbot.pid"
+authfile = os.getcwd() + "//makerbot.auth";
 
 
-class HandlerClass(SimpleHTTPRequestHandler):
-    def log_request(self, format, *args):
-        pass
-
-
-class WebThread(threading.Thread):
+class CamThread(threading.Thread):
+    def __init__(self, ip):
+        threading.Thread.__init__(self)
+        self.ip = ip
     def run(self):
-        global httpd
-        httpd = ServerClass((ip, port), HandlerClass)
-        sa = httpd.socket.getsockname()
-        print "Serving HTTP on", sa[0], "port", sa[1], "..."
-        logging.getLogger("requests").setLevel(logging.WARNING)
-        httpd.serve_forever()
+        global connection
+        while True:
+            ip_octet = re.search('(?:\d{1,3}\.\d{1,3}\.)(\d{1,3}\.\d{1,3})', self.ip).group(1)
+            if makerbot:
+                WriteJSON(ip_octet)
+                makerbot.save_camera_png(os.getcwd() + "\\makerbot-" + ip_octet + ".png")
+                time.sleep(0.025)
+            else:
+                print "Lost connection to", self.ip, "..."
+                if len(connection) > 1:
+                    self.exit()
+                else:
+                    Shutdown()
 
 
-def MakerBotConnect(ip, auth, server):
-    global makerbot
+def MakerBotConnect(ip, auth):
+    global makerbot, connection
     try:
         print "Connecting to", ip, "..."
         makerbot = makerbotapi.Makerbot(ip, auth_code=auth)
+        
+        connected = False
         
         if auth == None:
             print "Authenticate with your MakerBot Replicator ..."
@@ -48,38 +54,56 @@ def MakerBotConnect(ip, auth, server):
                 makerbot.authenticate_fcgi()
                 print "Authenticated:", makerbot.auth_code
             except makerbotapi.makerbotapi.AuthenticationError as auth_error:
-                print "Authentication not accepted ..."
-                ShutdownWebcam()
+                if len(connection) > 1:
+                    print "Authentication not accepted for", ip ,"..."
+                    return
+                else:
+                    print "Authentication not accepted ..."
+                    Shutdown()
         else:
+            connected = True
             print "Authenticated Accepted!"
             
-        makerbot.authenticate_json_rpc()
-        print "Connected Successfully!"
-        if server:
-            WebThread().start()
+        if connected:
+            makerbot.authenticate_json_rpc()
+            print "Connected Successfully!\n"
+            CamThread(ip).start()
+
     except socket_error as serr:
         if serr.errno == 10060:
-            print "Socket Error: Check the IP Address and open ports."
-            ShutdownWebcam()
+            print "Socket Error: Cannot connect to", ip, "\n"
+            if len(connection) > 1:
+                return
+            else:
+                Shutdown()
 
 
-def ShutdownWebcam():
+def Shutdown():
     print "Shutting Down ..."
-    if httpd:
-        httpd.shutdown()
-        print "HTTP Server stopped ..."
     os.unlink(pidfile)
     sys.exit()
 
 
-def WriteJSON():
-    with open("makerbot.json", "w") as rawjson:
-        rawjson.write(json.dumps(
-            makerbot.rpc_request_response(
-                'get_system_information',
-                {'username': 'conveyor'}
-            )
-        ))
+def WriteJSON(ip):
+    with open("makerbot-" + ip + ".json", "w") as rawjson:
+        try:
+            rawjson.write(json.dumps(
+                makerbot.rpc_request_response(
+                    'get_system_information',
+                    {'username': 'conveyor'}
+                )
+            ))
+        except:
+            CamThread(ip).exit()
+
+
+def AuthenticationFile():
+    global authfile
+    authconfig = json.loads(open(authfile).read())
+    for printer in authconfig['printers']:
+        connection.append(printer['ip'])
+    for printer in authconfig['printers']:
+        MakerBotConnect(printer['ip'], printer['auth'])
 
 
 if __name__ == '__main__':
@@ -88,35 +112,21 @@ if __name__ == '__main__':
         sys.exit()
     else:
         file(pidfile, 'w').write(pid)
-        
-    if len(sys.argv) < 2:
-        print "args: <ip address> [auth code] [web server]"
-        ShutdownWebcam()
 
-    makerbot_ip = sys.argv[1]
-    makerbot_auth = None
-    makerbot_server = False
-
-    if len(sys.argv) == 3:
-        if sys.argv[2].lower() == 'true':
-            makerbot_server = True
+    if os.path.isfile(authfile):
+        print "MakerBot Authentication file found ...\n"
+        AuthenticationFile()
+    else:
+        if len(sys.argv) < 2:
+            print "No MakerBot Authentication file found ..."
+            print "args: <ip address> [auth code]"
+            sys.exit()
         else:
-            makerbot_auth = sys.argv[2]
+            makerbot_ip = sys.argv[1]
+            makerbot_auth = None
+            makerbot_server = False
 
-    if len(sys.argv) == 4:
-        makerbot_auth = sys.argv[2]
-        if sys.argv[3].lower() == 'true':
-            makerbot_server = True
+            if len(sys.argv) == 3:
+                makerbot_auth = sys.argv[2]
 
-    try:
-        MakerBotConnect(makerbot_ip, makerbot_auth, makerbot_server)
-        while True:
-            if makerbot:
-                WriteJSON()
-                makerbot.save_camera_png(os.getcwd() + "\\makerbot.png")
-                time.sleep(0.025)
-            else:
-                ShutdownWebcam()
-
-    except KeyboardInterrupt:
-        ShutdownWebcam()
+            MakerBotConnect(makerbot_ip, makerbot_auth)
